946e47f4e17ecd46678bd7d915dbe81e21c8d64d
dev-nayandas <rich.nayan@gmail.com>
2022-03-26 15:20:34 +0600

    Header Added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='site-name'>Crazy Laptop Deals</h1>
+            <h2 className='to-do'>Choose 4 Laptops</h2>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Components/Product/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Product = (props) => {
+    const {img, id, name, price} = props.product;
+    return (
+        <div>
+            <h2>This is Products</h2>
+        </div>
+    );
+};
+
+export default Product;
